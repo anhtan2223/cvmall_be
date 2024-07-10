912afe8632ec896d59cf733ed8030493dd949d27
anhtan2223 <anhtan3332@gmail.com>
2024-07-11 00:11:31 +0700

    Import Employee By Excel

--- Application/Application.Core/Services/EmployeeServices.cs
+++ Application/Application.Core/Services/EmployeeServices.cs
@@ -9,6 +9,10 @@ using Application.Core.Interfaces.Core;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml;
 using Framework.Core.Helpers;
 using Framework.Core.Helpers.Excel;
@@ -254,8 +258,6 @@ namespace Application.Core.Services.Core
 
             return dataMapping;
         }
-
-
         public async Task<byte[]> ExportAllExcel()
         {
 
@@ -349,5 +351,166 @@ namespace Application.Core.Services.Core
         }
 
         #endregion
+        public async Task<int> Import(IFormFile file)
+        {
+            var count = 0;
+            using (var transaction = await _unitOfWork.BeginTransactionAsync())
+            {
+                try
+                {
+                    Dictionary<string, int> stateMappings = new Dictionary<string, int>
+                    {
+                        { "Đang làm việc", 0 },
+                        { "Đang thử việc", 1 },
+                        { "Đang thực tập", 2 },
+                        { "Đã nghỉ việc", 3 }
+                    };
+                    Dictionary<string, bool> marriedMappings = new Dictionary<string, bool>
+                    {
+                        { "Độc thân", false },
+                        { "Đã kết hôn", true }
+                    };
+                    string pattern = @"^VHEC-\d+$";
+                    Regex regex = new Regex(pattern);
+                    List<List<string>> excelData = ReadExcelData(file);
+                    // List<EmployeeRequest> listE = new List<EmployeeRequest>() ;
+                    foreach (var employee in excelData)
+                    {
+                        if(!int.TryParse(employee[0] , out count))
+                            continue ;
+                        foreach (var item in new List<int>{1,2,3,4,7,11})
+                        {
+                            if(employee[item] == null)
+                                throw new ArgumentException();
+                        }
+                        if(!stateMappings.ContainsKey(employee[7]))
+                            throw new ArgumentException();
+                        if(!marriedMappings.ContainsKey(employee[17]))
+                            throw new ArgumentException();
+                        
+                        var request = new EmployeeRequest{
+                            employee_code       = employee[1] ,
+                            full_name           = employee[2] ,
+                            initial_name        = employee[3] ,
+                            branch              = employee[4] ,
+                            current_group       = ""          ,
+                            EmployeeDepartments = (employee[5] == null) ? null : GetDepartmentFromString(employee[5]) ,
+                            EmployeePositions   = (employee[6] == null) ? null : GetPositionFromString(employee[6]) ,
+                            state               = stateMappings[employee[7]] ,
+                            phone               = employee[8] ,
+                            company_email       = employee[9] ,
+                            personal_email      = employee[10] ,
+                            birthday            = StringToDateTime(employee[11]) ,
+                            permanent_address   = employee[12] ,
+                            current_address     = employee[13] ,
+                            id_number           = employee[14] ,
+                            date_issue          = (employee[15] == null) ? null : StringToDateTime(employee[15]) ,
+                            location_issue      = employee[16] ,
+                            is_married          = marriedMappings[employee[17]]
+                        };
+
+                        var isValidEmployeeCode = !await employeeRepository
+                                                    .GetQuery()
+                                                    .AnyAsync(x => x.employee_code == request.employee_code);
+                                                    
+                        if(!isValidEmployeeCode || !regex.IsMatch(request.employee_code))
+                            throw new ArgumentException();
+
+                        var Employee = _mapper.Map<Employee>(request);
+                        await employeeRepository.AddEntityAsync(Employee) ;
+                        await _unitOfWork.SaveChangesAsync();
+                    }
+                    
+                    transaction.Commit();
+                    count = 0 ;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+            return count ;
+        }
+        private DateTime StringToDateTime(string input)
+        {
+            long dateType ;
+            if(long.TryParse(input ,out dateType))
+                return new DateTime(1900,1,1).AddDays(dateType - 2);
+
+           return DateTime.ParseExact(input,"dd/MM/yyyy",CultureInfo.InvariantCulture);
+        }
+        private List<EmployeePositionRequest> GetPositionFromString(string input)
+        {
+            var result = new List<EmployeePositionRequest>() ;
+            var list  = input.Split(",");
+            var position = _unitOfWork
+                                .GetRepository<Position>()
+                                .GetQuery()
+                                .ExcludeSoftDeleted()
+                                .Where(x => list.Contains(x.name));
+            foreach (var p in position)
+            {
+                result.Add(new EmployeePositionRequest{
+                    position_id = p.id 
+                });
+            }
+            return result ;
+        }
+        private List<EmployeeDepartmentRequest> GetDepartmentFromString(string input)
+        {
+            var result = new List<EmployeeDepartmentRequest>() ;
+            var list  = input.Split(",");
+            var department = _unitOfWork
+                                .GetRepository<Department>()
+                                .GetQuery()
+                                .ExcludeSoftDeleted()
+                                .Where(x => list.Contains(x.name));
+            foreach (var d in department)
+            {
+                result.Add(new EmployeeDepartmentRequest{
+                    department_id = d.id 
+                });
+            }
+            return result ;
+        }
+        private List<List<string>> ReadExcelData(IFormFile file)
+        {
+            List<List<string>> data = new List<List<string>>();
+
+            using (var stream = new MemoryStream())
+            {
+                file.CopyTo(stream);
+                stream.Position = 0;
+
+                using (SpreadsheetDocument document = SpreadsheetDocument.Open(stream, false))
+                {
+                    WorkbookPart workbookPart = document.WorkbookPart;
+                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                    SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+                    foreach (Row row in sheetData.Elements<Row>())
+                    {
+                        List<string> rowData = new List<string>();
+                        foreach (Cell cell in row.Elements<Cell>())
+                        {
+                            rowData.Add(GetCellValue(document, cell));
+                        }
+                        data.Add(rowData);
+                    }
+                }
+            }
+            return data;
+        }
+        private string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            string value = cell.CellValue?.InnerText;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return document.WorkbookPart.SharedStringTablePart.SharedStringTable
+                    .Elements<SharedStringItem>().ElementAt(int.Parse(value)).InnerText;
+            }
+            return value;
+        }
     }
 }
